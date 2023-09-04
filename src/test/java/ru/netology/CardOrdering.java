6c20f8b3171d93757893ad7774ad8d94717a3b24
Demitros <serrasalmus.80@gmail.com>
2023-09-04 15:50:08 +0500

    added

--- src/test/java/ru/netology/CardOrdering.java
+++ src/test/java/ru/netology/CardOrdering.java
@@ -1,17 +1,14 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -40,7 +37,6 @@ class CardOrderingTest {
     @Test
     void testPositiveAllInput() {
         driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79512852525");
         driver.findElement(By.className("checkbox__box")).click();
@@ -51,7 +47,6 @@ class CardOrderingTest {
 
     @Test
     void shouldCardFormInvalidName() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivan Petrov"); // Имя набрано латиницей
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79512852525");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
