090a7e13690fbf4ccf4cdb9c39821d87357c7424
Demitros <serrasalmus.80@gmail.com>
2023-09-06 09:09:17 +0500

    added

--- src/test/java/ru/netology/CardOrdering.java
+++ src/test/java/ru/netology/CardOrdering.java
@@ -38,72 +38,72 @@ class CardOrderingTest {
     }
 
     @Test
-    void testPositiveAllInput() {
-        driver.findElement(By.cssSelector("[data-test-id=`name`] input")).sendKeys("Иван Петров-Иванов");
-        driver.findElement(By.cssSelector("[data-test-id=`phone`] input")).sendKeys("+79512852525");
+    public void testPositiveAllInput() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Иванов");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79512852525");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=`order-success`]")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @Test
-    void shouldCardFormInvalidName() {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivan Petrov"); // Имя набрано латиницей
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79512852525");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+    public void shouldCardFormInvalidName() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivan Petrov"); // Имя набрано латиницей
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79512852525");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button")).click();
 
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
 
         assertEquals(expected, actual);
     }
 
     @Test
     void shouldCardFormInvalidPhone() {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+795128525252");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+795128525252");
         driver.findElement(By.className("button")).click();
 
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
 
         assertEquals(expected, actual);
     }
 
     @Test
     void shouldCardFormWithoutName() { // В заявке отсутствует имя
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79512852525");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79512852525");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button")).click();
 
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
 
         assertEquals(expected, actual);
     }
 
     @Test
     void shouldCardFormWithoutPhone() { // В заявке отсутствует номер телефона
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button")).click();
 
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=`phone`].input_invalid .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
 
         assertEquals(expected, actual);
     }
 
     @Test
     void shouldCardFormWithoutCheckbox() { // В заявке не проставлен чек-бокс
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79512852525");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79512852525");
         driver.findElement(By.className("button")).click();
 
         String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=`agreement`].input_invalid .checkbox__text")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid .checkbox__text")).getText().trim();
 
         assertEquals(expected, actual);
     }
